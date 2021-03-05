7214a164b3024d1f7ab999689a52eeb6d0422f0f
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2021-03-05 13:36:20 +0100

    made if statement

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+var spelerAanZet = 2;
+
+
 function neerzetten() {
   var mijnblokje = document.getElementById("blokje1");
-  mijnblokje.innerHTML = "O";
+  if( spelerAanZet == 1 ) {
+     mijnblokje.innerHTML = "O";
+     spelerAanZet = 2;
+  } else {
+    mijnblokje.innerHTML = "X";
+    spelerAanZet = 1;
+  }
 }
