f0553b2c553c35d64d93d6ee594018833a681807
KariinDeLeeuw <65500409+KariinDeLeeuw@users.noreply.github.com>
2021-03-07 18:31:54 +0100

    changed styling

--- script.js
+++ script.js
@@ -4,84 +4,84 @@ var spelerAanZet = 2;
 function neerzetten( blokjesnummer ) {
   var mijnBlokje = document.getElementById("blokje" + blokjesnummer);
   var inhoudElement = mijnBlokje.innerHTML;
-  if( inhoudElement != "<p>.</p>" ) {
+  if( inhoudElement != "." ) {
     alert("Dit mag niet!");
   } else {
     if( spelerAanZet == 1 ) {
-      mijnBlokje.innerHTML = "O";
-      inhoudElement = "O"
+      mijnBlokje.innerHTML = "❍";
+      inhoudElement = "❍";
       spelerAanZet = 2;
    } else {
-     mijnBlokje.innerHTML = "X";
-     inhoudElement = "X";
+     mijnBlokje.innerHTML = "✕";
+     inhoudElement = "✕";
      spelerAanZet = 1;
    }
  }
- if(document.getElementById("blokje1").innerHTML == "X"
- && document.getElementById("blokje2").innerHTML == "X"
- && document.getElementById("blokje3").innerHTML == "X"
+ if(document.getElementById("blokje1").innerHTML == "✕"
+ && document.getElementById("blokje2").innerHTML == "✕"
+ && document.getElementById("blokje3").innerHTML == "✕"
  ||
- document.getElementById("blokje4").innerHTML == "X"
- && document.getElementById("blokje5").innerHTML == "X"
- && document.getElementById("blokje6").innerHTML == "X"
+ document.getElementById("blokje4").innerHTML == "✕"
+ && document.getElementById("blokje5").innerHTML == "✕"
+ && document.getElementById("blokje6").innerHTML == "✕"
  ||
- document.getElementById("blokje7").innerHTML == "X"
- && document.getElementById("blokje8").innerHTML == "X"
- && document.getElementById("blokje9").innerHTML == "X"
+ document.getElementById("blokje7").innerHTML == "✕"
+ && document.getElementById("blokje8").innerHTML == "✕"
+ && document.getElementById("blokje9").innerHTML == "✕"
  ||
- document.getElementById("blokje1").innerHTML == "X"
- && document.getElementById("blokje4").innerHTML == "X"
- && document.getElementById("blokje7").innerHTML == "X"
+ document.getElementById("blokje1").innerHTML == "✕"
+ && document.getElementById("blokje4").innerHTML == "✕"
+ && document.getElementById("blokje7").innerHTML == "✕"
  ||
- document.getElementById("blokje2").innerHTML == "X"
- && document.getElementById("blokje5").innerHTML == "X"
- && document.getElementById("blokje8").innerHTML == "X"
+ document.getElementById("blokje2").innerHTML == "✕"
+ && document.getElementById("blokje5").innerHTML == "✕"
+ && document.getElementById("blokje8").innerHTML == "✕"
 ||
-document.getElementById("blokje3").innerHTML == "X"
- && document.getElementById("blokje6").innerHTML == "X"
- && document.getElementById("blokje9").innerHTML == "X"
+document.getElementById("blokje3").innerHTML == "✕"
+ && document.getElementById("blokje6").innerHTML == "✕"
+ && document.getElementById("blokje9").innerHTML == "✕"
  ||
- document.getElementById("blokje1").innerHTML == "X"
- && document.getElementById("blokje5").innerHTML == "X"
- && document.getElementById("blokje9").innerHTML == "X"
+ document.getElementById("blokje1").innerHTML == "✕"
+ && document.getElementById("blokje5").innerHTML == "✕"
+ && document.getElementById("blokje9").innerHTML == "✕"
  ||
- document.getElementById("blokje3").innerHTML == "X"
- && document.getElementById("blokje5").innerHTML == "X"
- && document.getElementById("blokje7").innerHTML == "X")
+ document.getElementById("blokje3").innerHTML == "✕"
+ && document.getElementById("blokje5").innerHTML == "✕"
+ && document.getElementById("blokje7").innerHTML == "✕")
  {
-   alert("X heeft gewonnen!");
- } else if(document.getElementById("blokje1").innerHTML == "O"
- && document.getElementById("blokje2").innerHTML == "O"
- && document.getElementById("blokje3").innerHTML == "O"
+   alert("✕ heeft gewonnen!");
+ } else if(document.getElementById("blokje1").innerHTML == "❍"
+ && document.getElementById("blokje2").innerHTML == "❍"
+ && document.getElementById("blokje3").innerHTML == "❍"
  ||
- document.getElementById("blokje4").innerHTML == "O"
- && document.getElementById("blokje5").innerHTML == "O"
- && document.getElementById("blokje6").innerHTML == "O"
+ document.getElementById("blokje4").innerHTML == "❍"
+ && document.getElementById("blokje5").innerHTML == "❍"
+ && document.getElementById("blokje6").innerHTML == "❍"
  ||
- document.getElementById("blokje7").innerHTML == "O"
- && document.getElementById("blokje8").innerHTML == "O"
- && document.getElementById("blokje9").innerHTML == "O"
+ document.getElementById("blokje7").innerHTML == "❍"
+ && document.getElementById("blokje8").innerHTML == "❍"
+ && document.getElementById("blokje9").innerHTML == "❍"
  ||
- document.getElementById("blokje1").innerHTML == "O"
- && document.getElementById("blokje4").innerHTML == "O"
- && document.getElementById("blokje7").innerHTML == "O"
+ document.getElementById("blokje1").innerHTML == "❍"
+ && document.getElementById("blokje4").innerHTML == "❍"
+ && document.getElementById("blokje7").innerHTML == "❍"
  ||
- document.getElementById("blokje2").innerHTML == "O"
- && document.getElementById("blokje5").innerHTML == "O"
- && document.getElementById("blokje8").innerHTML == "X"
+ document.getElementById("blokje2").innerHTML == "❍"
+ && document.getElementById("blokje5").innerHTML == "❍"
+ && document.getElementById("blokje8").innerHTML == "❍"
 ||
-document.getElementById("blokje3").innerHTML == "O"
- && document.getElementById("blokje6").innerHTML == "O"
- && document.getElementById("blokje9").innerHTML == "O"
+document.getElementById("blokje3").innerHTML == "❍"
+ && document.getElementById("blokje6").innerHTML == "❍"
+ && document.getElementById("blokje9").innerHTML == "❍"
  ||
- document.getElementById("blokje1").innerHTML == "O"
- && document.getElementById("blokje5").innerHTML == "O"
- && document.getElementById("blokje9").innerHTML == "O"
+ document.getElementById("blokje1").innerHTML == "❍"
+ && document.getElementById("blokje5").innerHTML == "❍"
+ && document.getElementById("blokje9").innerHTML == "❍"
  ||
- document.getElementById("blokje3").innerHTML == "O"
- && document.getElementById("blokje5").innerHTML == "O"
- && document.getElementById("blokje7").innerHTML == "O"){
-   alert("O heeft gewonnen!")
+ document.getElementById("blokje3").innerHTML == "❍"
+ && document.getElementById("blokje5").innerHTML == "❍"
+ && document.getElementById("blokje7").innerHTML == "❍"){
+   alert("❍ heeft gewonnen!")
  }
 }
   
